99643d2e876d1193f6bbb1b285ba4543911f4018
zhihaoguo <zhihaoguo@microsoft.com>
2021-03-02 16:14:40 +0800

    clean code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,21 +1,10 @@
 package org.example;
 
 
-
-import com.microsoft.azure.spring.integration.core.AzureHeaders;
-import com.microsoft.azure.spring.integration.core.api.reactor.AzureCheckpointer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.messaging.Message;
-
-import java.util.function.Consumer;
-
-
-
-
 
 @SpringBootApplication
 public class App {
@@ -26,16 +15,4 @@ public class App {
 
     }
 
-    @Bean
-    public Consumer<Message<String>> consume() {
-        return message -> {
-            AzureCheckpointer checkpointer = (AzureCheckpointer) message.getHeaders().get(AzureHeaders.CHECKPOINTER);
-
-            LOGGER.info("Message is consuming, message.getHeaders()=" + message.getHeaders() + "message.getPayload()" + message.getPayload());
-            checkpointer.success()
-                        .doOnSuccess(success -> LOGGER.info("Message '{}' successfully checkpointed", message.getPayload()))
-                        .doOnError(error -> LOGGER.error("Exception: {}", error.getMessage()))
-                        .subscribe();
-        };
-    }
 }
